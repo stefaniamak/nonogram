f618ba4690fe80b6c278d86b1692edd6ea5b19d0
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2023-11-20 18:31:26 +0200

    creates static nonogram state

--- lib/pages/game/home_page.dart
+++ lib/pages/game/home_page.dart
@@ -1,24 +1,29 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nonogram/backend/models/nonogram.dart';
+import 'package:nonogram/game_loop/nonogram_state.dart';
 import 'package:nonogram/nono_tabs_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   static const String route = '/home';
 
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: body(context));
-    return NonoTabsPage(
-      body: body(context),
-    );
-  }
-
-  Column body(BuildContext context) {
     Nonogram draftNono = Nonogram((n) => n
       ..columns = 10
       ..rows = 10);
+    var nonogramState = useNonogramState(draftNono);
+    return Scaffold(body: body(context, nonogramState));
+    return NonoTabsPage(
+      body: body(context, nonogramState),
+    );
+  }
+
+  Column body(BuildContext context, NonogramState nonogramState) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -26,7 +31,7 @@ class HomePage extends StatelessWidget {
         Center(
           child: CustomPaint(
             size: Size(400, 400),
-            painter: NonogramGrid(nonogram: draftNono),
+            painter: NonogramGrid(nonogramState: nonogramState),
           ),
         ),
       ],
@@ -37,46 +42,30 @@ class HomePage extends StatelessWidget {
 enum PointState { empty, filled, cross }
 
 class NonogramGrid extends CustomPainter {
-  final Nonogram nonogram;
+  final NonogramState nonogramState;
 
-  NonogramGrid({super.repaint, required this.nonogram});
+  NonogramGrid({super.repaint, required this.nonogramState});
 
   @override
   void paint(Canvas canvas, Size size) {
+    var nonogram = nonogramState.nonogram;
     double side = size.width / nonogram.columns;
 
     print('side: $side');
 
-    Map<String, PointState> pS1 = {
-      '{0: 0}': PointState.cross,
-      '{0: 2}': PointState.cross,
-      '{1: 1}': PointState.filled,
-      '{2: 2}': PointState.empty,
-    };
-
     for (var r = 0; r < nonogram.rows; r++) {
       for (var c = 0; c < nonogram.columns; c++) {
-        // print('pS1: ${pS1}');
-        // print('{r: c}: ${{r: c}}');
-        // print('pS1[{r: c}]: ${pS1[{r: c}.toString()]}');
-        // print('pS1[{0: 0}]: ${pS1[{0: 0}.toString()]}');
-        switch (pS1[{r: c}.toString()] ?? PointState.empty) {
+        switch (nonogramState.activeSolution[Point(r, c)] ?? PointState.empty) {
           case PointState.empty:
-            {
-              drawEmptyBox(canvas, Size(r * side, c * side), side);
-            }
+            drawEmptyBox(canvas, Size(r * side, c * side), side);
             break;
           case PointState.filled:
-            {
-              print('filled');
-              drawFilledBox(canvas, Size(r * side, c * side), side);
-            }
+            drawFilledBox(canvas, Size(r * side, c * side), side);
             break;
           case PointState.cross:
             drawCrossBox(canvas, Size(r * side, c * side), side);
+            break;
         }
-
-        // drawEmptyBox(canvas, Size(r * side, c * side), side);
       }
     }
   }
