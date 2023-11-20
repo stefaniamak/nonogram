5757dbd5c9fe01885b4ef5140b36b1cd7f976fb7
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2023-11-20 16:35:46 +0200

    makes draft grid

--- lib/pages/game/home_page.dart
+++ lib/pages/game/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nonogram/backend/models/nonogram.dart';
 import 'package:nonogram/nono_tabs_page.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,30 +9,68 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: body());
+    return Scaffold(body: body(context));
     return NonoTabsPage(
-      body: body(),
+      body: body(context),
     );
   }
 
-  Column body() {
-    return const Column(
+  Column body(BuildContext context) {
+    Nonogram draftNono = Nonogram((n) => n
+      ..columns = 10
+      ..rows = 10);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text('game home pageeeeeeeeee'),
-        Expanded(
-          child: Text('drowing'),
-          // child: SizedBox(
-          //   width: 200, //double.infinity,
-          //   height: 200, //double.infinity,
-          //   child: GridPaper(
-          //     color: Colors.black,
-          //     interval: 100,
-          //     divisions: 1,
-          //     subdivisions: 5,
-          //   ),
-          // ),
+        Center(
+          child: CustomPaint(
+            size: Size(400, 400),
+            painter: NonogramGrid(nonogram: draftNono),
+          ),
         ),
       ],
     );
+  }
+}
+
+class NonogramGrid extends CustomPainter {
+  final Nonogram nonogram;
+
+  NonogramGrid({super.repaint, required this.nonogram});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double side = size.width / nonogram.columns;
+
+    print('side: $side');
+
+    for (var r = 0; r < nonogram.rows; r++) {
+      for (var c = 0; c < nonogram.columns; c++) {
+        drawEmptyBox(canvas, Size(r * side, c * side), side);
+      }
+    }
+  }
+
+  void drawEmptyBox(Canvas canvas, Size s, double side) {
+    var paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    var path = Path();
+    path.moveTo(s.width, s.height);
+    path.lineTo(s.width + side, s.height);
+    path.lineTo(s.width + side, s.height + side);
+    path.lineTo(s.width, s.height + side);
+    path.lineTo(s.width, s.height);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+    throw UnimplementedError();
   }
 }
