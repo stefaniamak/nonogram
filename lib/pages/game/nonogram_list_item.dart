4f548a944631df200f7febbbd3ef8da3b3c42f5d
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-03-24 16:08:40 +0200

    refactors list item widget

---
+++ lib/pages/game/nonogram_list_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:nonogram/backend/models/nonogram.dart';
+import 'package:nonogram/pages/game/nonogram_grid_and_clues.dart';
+
+class NonogramListItem extends StatelessWidget {
+  const NonogramListItem({
+    super.key,
+    required this.nonogram,
+  });
+
+  final Nonogram nonogram;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.withOpacity(0.5),
+      child: Column(
+        children: [
+          NonogramGridAndClues(
+            nonogram: nonogram,
+            maxSize: const Size(250, 250),
+          ),
+          Text(nonogram.info?.title ?? '-'),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/game/nonogram_list_page.dart
+++ lib/pages/game/nonogram_list_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nonogram/backend/models/nonogram.dart';
 import 'package:nonogram/game_loop/nonogram_list_state.dart';
-import 'package:nonogram/pages/game/nonogram_grid_and_clues.dart';
+import 'package:nonogram/pages/game/nonogram_list_item.dart';
 
 class NonogramListPage extends HookWidget {
   static const String route = '/puzzles';
@@ -19,18 +19,7 @@ class NonogramListPage extends HookWidget {
             delegate: SliverChildBuilderDelegate(
               (context, index) {
                 Nonogram nonogram = nonogramListState.nonograms[index];
-                return Container(
-                  color: Colors.grey,
-                  child: Column(
-                    children: [
-                      NonogramGridAndClues(
-                        nonogram: nonogram,
-                        maxSize: const Size(250, 250),
-                      ),
-                      Text(nonogram.info?.title ?? '-'),
-                    ],
-                  ),
-                );
+                return NonogramListItem(nonogram: nonogram);
               },
               childCount: nonogramListState.nonograms.length,
             ),
