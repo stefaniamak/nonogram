559e3bcf3471730baf57f1136450961b158ca785
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-03-27 22:15:01 +0200

    adds swing

--- lib/backend/database/nonograms.dart
+++ lib/backend/database/nonograms.dart
@@ -1,4 +1,5 @@
 import 'package:nonogram/backend/database/nonograms/2_cat.dart';
+import 'package:nonogram/backend/database/nonograms/529_swing.dart';
 import 'package:nonogram/backend/models/nonogram.dart';
 
 import 'nonograms/1_dancer.dart';
@@ -7,8 +8,10 @@ class Nonograms {
   List<Nonogram> get allNonograms => [
         dancer,
         cat,
+        swing,
       ];
 
   Nonogram get dancer => kDancer;
   Nonogram get cat => kCat;
+  Nonogram get swing => kSwing;
 }
