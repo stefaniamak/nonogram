290a352da1c67d4b69dc104f9d1d841f818e7073
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-08-23 20:31:30 +0300

    updates wording

--- lib/solver/line_solver.dart
+++ lib/solver/line_solver.dart
@@ -13,9 +13,9 @@ import '../backend/type_extensions/nono_direction_extension.dart';
 class LineSolver {
   bool kPrintComments = false;
   bool activateReturnOnNotEnoughSolvedLines = false;
-  bool countBoxes = true;
-  bool countActualBoxes = true;
-  bool countOtherBoxes = true;
+  bool countBoxes = false;
+  bool countActualBoxes = false;
+  bool countOtherBoxes = false;
   bool groupSteps = true;
 
   void solve(NonogramState state) async {
@@ -322,10 +322,10 @@ class LineSolver {
     List<List<String>> possibleSolutions = Iterable.generate(line.length, (_) => <String>[]).toList();
     for (int clueIndex = 0; clueIndex < clues.length; clueIndex++) {
       int minStartingPoint = clueIndex == 0 ? 0 : clues.take(clueIndex).reduce((int value, int element) => value + element + 1);
-      int maxEndingPoint = clueIndex == clues.length - 1
+      int maxStartingPoint = clueIndex == clues.length - 1
           ? line.length
           : line.length - clues.sublist(clueIndex + 1).reduce((int value, int element) => value + element + 1) - clues[clueIndex];
-      for (int charIndex = minStartingPoint; charIndex < maxEndingPoint; charIndex++) {
+      for (int charIndex = minStartingPoint; charIndex < maxStartingPoint; charIndex++) {
         bool? cache = state.cachedBoxSolutions['$clues,$clueIndex,$line,$charIndex'];
         bool isInCache = cache != null;
         bool result;
