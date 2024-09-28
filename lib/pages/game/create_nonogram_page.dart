0763b400d0e838e2eff0b1999d3e36154bc86f61
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-09-28 17:08:11 +0300

    fixes create nonogram screen with new models

--- lib/pages/game/create_nonogram_page.dart
+++ lib/pages/game/create_nonogram_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nonogram/backend/cubits/nonogram_solver_cubit/nonogram_solver_cubit.dart';
+import 'package:nonogram/backend/models/isolate/isolate_clues.dart';
+import 'package:nonogram/backend/models/isolate/isolate_nonogram.dart';
 import 'package:nonogram/game_loop/create_nonogram_state.dart';
+import 'package:nonogram/pages/game/nonogram_grid_and_clues.dart';
+import 'package:nonogram/pages/game/nonogram_page.dart';
 
 class CreateNonogramPage extends HookWidget {
   const CreateNonogramPage({
@@ -24,47 +30,46 @@ class CreateNonogramPage extends HookWidget {
             },
           ),
           // TODO(stef): restore
-          // Expanded(
-          //   flex: 10,
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.end,
-          //     children: [
-          //       Expanded(
-          //         child: Center(
-          //           child: NonogramGridAndClues(
-          //             // nonogram: nonogramState.nonogram,
-          //             clues: Clues(
-          //               (c) => c
-          //                 ..columns = ListBuilder(state.horizontalClues)
-          //                 ..rows = ListBuilder(state.verticalClues),
-          //             ),
-          //             onLineTap: (Axis axis, int index, List<int> cluesLine) {
-          //               state.setSelectedLine(axis, index, cluesLine);
-          //             },
-          //             padding: const EdgeInsets.all(32),
-          //             boxItems: Size(state.width + 0, state.height + 0),
-          //             maxSize: Size(
-          //               MediaQuery.of(context).size.width * 0.8,
-          //               MediaQuery.of(context).size.height * 0.7,
-          //             ),
-          //           ),
-          //         ),
-          //       ),
-          //       RotatedBox(
-          //         quarterTurns: 1,
-          //         child: Slider(
-          //           value: state.height + 0.0,
-          //           min: 1,
-          //           max: 50,
-          //           divisions: 50,
-          //           onChanged: (value) {
-          //             state.updateHeight(value.ceil());
-          //           },
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
+          Expanded(
+            flex: 10,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Center(
+                    child: NonogramGridAndClues(
+                      // nonogram: nonogramState.nonogram,
+                      clues: IsolateClues(
+                        columns: state.horizontalClues,
+                        rows: state.verticalClues,
+                      ),
+                      onLineTap: (Axis axis, int index, List<int> cluesLine) {
+                        state.setSelectedLine(axis, index, cluesLine);
+                      },
+                      padding: const EdgeInsets.all(32),
+                      boxItems: Size(state.width + 0, state.height + 0),
+                      maxSize: Size(
+                        MediaQuery.of(context).size.width * 0.8,
+                        MediaQuery.of(context).size.height * 0.7,
+                      ),
+                    ),
+                  ),
+                ),
+                RotatedBox(
+                  quarterTurns: 1,
+                  child: Slider(
+                    value: state.height + 0.0,
+                    min: 1,
+                    max: 50,
+                    divisions: 50,
+                    onChanged: (value) {
+                      state.updateHeight(value.ceil());
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: Slider(
               value: state.width + 0.0,
@@ -77,27 +82,27 @@ class CreateNonogramPage extends HookWidget {
             ),
           ),
           // TODO(stef): restore
-          // OutlinedButton(
-          //   onPressed: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute<void>(
-          //         builder: (BuildContext context) => NonogramPage(
-          //           nonogram: Nonogram((n) => n
-          //             ..id = "-"
-          //             ..info = NonogramInfo((i) => i..title = "Custom").toBuilder()
-          //             ..note = "published,definitely unique,definitely line/color solvable"
-          //             ..clues = Clues(
-          //               (c) => c
-          //                 ..columns = ListBuilder(state.horizontalClues)
-          //                 ..rows = ListBuilder(state.verticalClues),
-          //             ).toBuilder()),
-          //         ),
-          //       ),
-          //     );
-          //   },
-          //   child: const Text('SOLVE'),
-          // ),
+          OutlinedButton(
+            onPressed: () {
+              final nonogram = IsolateNonogram(
+                id: "-",
+                clues: IsolateClues(
+                  columns: state.horizontalClues,
+                  rows: state.verticalClues,
+                ),
+              );
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => BlocProvider(
+                    create: (_) => NonogramSolverCubit()..initialize(nonogram: nonogram),
+                    child: NonogramPage(nonogram: nonogram),
+                  ),
+                ),
+              );
+            },
+            child: const Text('SOLVE'),
+          ),
           SizedBox(height: MediaQuery.of(context).padding.bottom + 12),
         ],
       ),
