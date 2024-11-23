f5e05f588665df71537d76dc28a0d04255fedc98
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-11-23 23:21:39 +0200

    refactoring of nonogram grid methods

--- lib/painters/nonogram_grid.dart
+++ lib/painters/nonogram_grid.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:nonogram/painters/grid_painter.dart';
 
 class NonogramGrid extends StatefulWidget {
-
   const NonogramGrid({
     required this.gridItemSide,
     required this.size,
@@ -28,11 +27,26 @@ class NonogramGrid extends StatefulWidget {
 }
 
 class _NonogramGridState extends State<NonogramGrid> {
-  bool _calceledOnTap = false;
+  bool _canceledOnTap = false;
 
   int getIndex(Offset position) {
     return (position.dx / widget.gridItemSide).floor() +
         (position.dy / widget.gridItemSide).floor() * widget.boxItems.width.floor();
+  }
+
+  void _handleTap(Offset position) {
+    widget.onTap?.call(getIndex(position));
+    setState(() => _canceledOnTap = false);
+  }
+
+  void _handlePan(Offset position) {
+    widget.onPan?.call(getIndex(position));
+    if (_canceledOnTap) _canceledOnTap = false;
+  }
+
+  void _handlePanEnd(Offset position) {
+    widget.onPanEnd?.call(getIndex(position));
+    setState(() => _canceledOnTap = false);
   }
 
   @override
@@ -41,44 +55,14 @@ class _NonogramGridState extends State<NonogramGrid> {
       width: widget.size.width,
       height: widget.size.height,
       child: GestureDetector(
-        // behavior: HitTestBehavior.deferToChild,
-        onTapDown: (TapDownDetails details) {
-          widget.onTap?.call(getIndex(details.localPosition));
-          setState(() {
-            _calceledOnTap = false;
-          });
-        },
-        onTapCancel: () {
-          setState(() {
-            _calceledOnTap = true;
-          });
-        },
-        onVerticalDragStart: (DragStartDetails details) {
-          if (!_calceledOnTap) widget.onTap?.call(getIndex(details.localPosition));
-        },
-        onVerticalDragUpdate: (DragUpdateDetails details) {
-          widget.onPan?.call(getIndex(details.localPosition));
-          if (_calceledOnTap) _calceledOnTap = false;
-        },
-        onVerticalDragEnd: (DragEndDetails details) {
-          widget.onPanEnd?.call(getIndex(details.localPosition));
-          setState(() {
-            _calceledOnTap = false;
-          });
-        },
-        onPanStart: (DragStartDetails details) {
-          if (!_calceledOnTap) widget.onTap?.call(getIndex(details.localPosition));
-        },
-        onPanUpdate: (DragUpdateDetails details) {
-          widget.onPan?.call(getIndex(details.localPosition));
-          if (_calceledOnTap) _calceledOnTap = false;
-        },
-        onPanEnd: (DragEndDetails details) {
-          widget.onPanEnd?.call(getIndex(details.localPosition));
-          setState(() {
-            _calceledOnTap = false;
-          });
-        },
+        onTapDown: (TapDownDetails details) => _handleTap(details.localPosition),
+        onTapCancel: () => setState(() => _canceledOnTap = true),
+        onVerticalDragStart: !_canceledOnTap ? (DragStartDetails details) => _handleTap(details.localPosition) : null,
+        onVerticalDragUpdate: (DragUpdateDetails details) => _handlePan(details.localPosition),
+        onVerticalDragEnd: (DragEndDetails details) => _handlePanEnd(details.localPosition),
+        onPanStart: (!_canceledOnTap) ? (DragStartDetails details) => _handleTap(details.localPosition) : null,
+        onPanUpdate: (DragUpdateDetails details) => _handlePan(details.localPosition),
+        onPanEnd: (DragEndDetails details) => _handlePanEnd(details.localPosition),
         child: CustomPaint(
           isComplex: true,
           painter: GridPainter(
