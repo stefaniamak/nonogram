9d762697d3025d6507fd2775252e603fdc1b116a
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-11-25 20:59:30 +0200

    more cofig (AppColors)

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -1,10 +1,22 @@
 import 'dart:ui';
 
+/// A class that defines the color palette used in the application.
 class AppColors {
+  /// White color with hex value `0xffffffff`.
   static const Color white = Color(0xffffffff);
+
+  /// Disabled color with hex value `0xffB3B3B3`.
   static const Color disabled = Color(0xffB3B3B3);
+
+  /// Black color with hex value `0xff000000`.
   static const Color black = Color(0xff000000);
+
+  /// Black color with 90% opacity and hex value `0xff1A1A1A`.
   static const Color black90 = Color(0xff1A1A1A);
+
+  /// Black color with 40% opacity and hex value `0xff666666`.
   static const Color black40 = Color(0xff666666);
+
+  /// Black color with 20% opacity and hex value `0xffCCCCCC`.
   static const Color black20 = Color(0xffCCCCCC);
 }
