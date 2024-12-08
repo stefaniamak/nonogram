df5f3564f9998e85875170e03a4721a914649ddf
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-12-08 23:14:14 +0200

    adds doc at nono string ext + removes one analys-opt

--- lib/backend/type_extensions/nono_list_extension.dart
+++ lib/backend/type_extensions/nono_list_extension.dart
@@ -1,35 +1,53 @@
 import 'package:nonogram/backend/type_extensions/nono_axis_extension.dart';
 
+/// Extension on List<String> to add utility methods.
 extension NonoStringListExtension on List<String> {
+  /// Checks if every element in the list is '0'.
   bool get everyElementIsZero => every((String e) => e == '0');
 }
 
+/// Extension on List<int> to add utility methods.
 extension NonoIntListExtension on List<int> {
+  /// Calculates the sum of all elements in the list.
   int get sum => fold<int>(0, (int previousValue, int element) => previousValue + element);
 }
 
+/// Extension on List<Map<int, NonoAxis>> to add utility methods.
 extension NonoMapListExtension on List<Map<int, NonoAxis>> {
+  /// Checks if a map with the given charIndex and lineType exists in the list.
+  ///
+  /// - [charIndex]: The character index to check.
+  /// - [lineType]: The line type to check.
+  ///
+  /// Returns True if the map exists in the list, false otherwise.
   bool isInStack(int charIndex, NonoAxis lineType) {
     return contains(<int, NonoAxis>{charIndex: lineType});
   }
 
+  /// Creates a list of new stack elements with the opposite axis of the given lineType.
+  ///
+  /// - [charIndexes]: The list of character indexes.
+  /// - [lineType]: The line type to determine the new axis.
+  ///
+  /// Returns A list of maps with the new axis.
   List<Map<int, NonoAxis>> getNewStackElements(List<int> charIndexes, NonoAxis lineType) {
     final NonoAxis newAxis = lineType == NonoAxis.row ? NonoAxis.column : NonoAxis.row;
-    final List<Map<int, NonoAxis>> newStackElements =
-        charIndexes.map((int charIndex) => <int, NonoAxis>{charIndex: newAxis}).toList();
-    return newStackElements;
+    return charIndexes.map((int charIndex) => <int, NonoAxis>{charIndex: newAxis}).toList();
   }
 
+  /// Updates the stack with the new stack elements.
+  ///
+  /// If an element already exists in the list, it is removed and then inserted at the middle.
+  /// If an element does not exist, it is added to the list.
+  ///
+  /// - [newStackElements]: The list of new stack elements to update.
+  ///
+  /// Returns The updated list.
   List<Map<int, NonoAxis>> updateStack(List<Map<int, NonoAxis>> newStackElements) {
-    // print('this: $this');
-    // print('newStackElements: $newStackElements');
-
     for (final Map<int, NonoAxis> element in newStackElements) {
-      // print('runn: $element');
-      int duplicateIndex =
+      final int duplicateIndex =
           indexWhere((Map<int, NonoAxis> e) => e.keys.first == element.keys.first && e.values.first == element.values.first);
       if (duplicateIndex >= 0) {
-        print('front?');
         remove(element);
         insert((duplicateIndex / 2).ceil(), element);
       } else {
--- lib/backend/type_extensions/nono_string_extension.dart
+++ lib/backend/type_extensions/nono_string_extension.dart
@@ -1,24 +1,44 @@
+/// Extension on String to add utility methods for nonogram puzzles.
 extension NonoStringExtension on String {
-  String characterAt(int index) => split('').toList().elementAt(index);
+  /// Returns the character at the specified index.
+  ///
+  /// - [index]: The index of the character to return.
+  ///
+  /// Returns the character at the specified index.
+  String characterAt(int index) => split('')[index];
 
-  int get sumFilledBoxes =>
-      split('').fold(0, (int previousValue, String element) => previousValue + (element != '?' ? int.parse(element) : 0));
+  /// Calculates the sum of all filled boxes in the string.
+  ///
+  /// Returns the sum of all filled boxes.
+  int get sumFilledBoxes => split('').fold(0, (int prev, String element) => prev + (element != '?' ? int.parse(element) : 0));
 
-  bool isSameClueIndexWith(String element) => (this == element) && this != '?' && element != '0' && this != '?' && element != '0';
+  /// Checks if the current string has the same clue index as the given element.
+  /// All clue indexes are from 2 and up.
+  ///
+  /// - [element]: The element to compare with.
+  ///
+  /// Returns true if the current string has the same clue index as the given element.
+  bool isSameClueIndexWith(String element) => this == element && this != '?' && element != '0';
 
-  String getRowIsolate(int lineIndex, int width) => split('')
-      .toList()
-      .getRange(lineIndex * width, width * (lineIndex + 1))
-      .join()
-      .replaceAll(' ', '')
-      .replaceAll('(', '')
-      .replaceAll(')', '')
-      .replaceAll(',', '');
+  /// Isolates a row from the string based on the given line index and width.
+  ///
+  /// - [lineIndex]: The index of the line to isolate.
+  /// - [width]: The width of the row.
+  ///
+  /// Returns the isolated row as a string.
+  String getRowIsolate(int lineIndex, int width) =>
+      split('').getRange(lineIndex * width, width * (lineIndex + 1)).join().replaceAll(RegExp(r'[ (),]'), '');
 
+  /// Isolates a column from the string based on the given line index and width.
+  ///
+  /// - [lineIndex]: The index of the line to isolate.
+  /// - [width]: The width of the column.
+  ///
+  /// Returns the isolated column as a string.
   String getColumnIsolate(int lineIndex, int width) {
     String columnSol = '';
-    for (int solChar = lineIndex; solChar < split('').toList().length; solChar = solChar + width) {
-      columnSol = '$columnSol${split('').toList().elementAt(solChar)}';
+    for (int solChar = lineIndex; solChar < length; solChar += width) {
+      columnSol += this[solChar];
     }
     return columnSol;
   }
