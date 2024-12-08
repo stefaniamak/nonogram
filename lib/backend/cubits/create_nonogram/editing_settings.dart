d7ad1d752d88db313e6dcfb3e837aba91dc14356
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-12-08 20:20:42 +0200

    refactors and add docs to rest of create nono cubits

--- lib/backend/cubits/create_nonogram/editing_settings.dart
+++ lib/backend/cubits/create_nonogram/editing_settings.dart
@@ -1,18 +1,33 @@
+/// Represents the settings for editing a Nonogram.
 class EditingSettings {
+  /// Creates a new instance of [EditingSettings].
   const EditingSettings({
     this.paint = false,
     this.erase = false,
     this.updateOnPanEnd = true,
   });
 
+  /// Whether the paint mode is enabled.
   final bool paint;
+
+  /// Whether the erase mode is enabled.
   final bool erase;
+
+  /// Whether to update on pan end.
   final bool updateOnPanEnd;
 
-  static EditingSettings get noMode => const EditingSettings();
-  static EditingSettings get paintMode => const EditingSettings(paint: true);
-  static EditingSettings get eraseMode => const EditingSettings(erase: true);
+  /// A constant instance of [EditingSettings] with no mode enabled.
+  static const EditingSettings noMode = EditingSettings();
 
+  /// A constant instance of [EditingSettings] with paint mode enabled.
+  static const EditingSettings paintMode = EditingSettings(paint: true);
+
+  /// A constant instance of [EditingSettings] with erase mode enabled.
+  static const EditingSettings eraseMode = EditingSettings(erase: true);
+
+  /// Creates a copy of the current [EditingSettings] with the given parameters.
+  ///
+  /// Returns a new instance of [EditingSettings] with the updated values.
   EditingSettings copyWith({
     bool? paint,
     bool? erase,
