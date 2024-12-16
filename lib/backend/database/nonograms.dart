d0afab4360355013b522ba01d9b6df293dd1d852
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-12-16 20:05:19 +0200

    refactors and adds doc to nonograms class

--- lib/backend/database/nonograms.dart
+++ lib/backend/database/nonograms.dart
@@ -4,18 +4,25 @@ import 'package:nonogram/backend/database/nonograms_for_isolate/knot_isolate.dar
 import 'package:nonogram/backend/database/nonograms_for_isolate/swing_isolate.dart';
 import 'package:nonogram/backend/models/isolate/isolate_nonogram.dart';
 
+/// A class that provides access to all nonograms.
 class Nonograms {
-  static List<IsolateNonogram> get all => <IsolateNonogram>[
-        dancer,
-        cat,
-        knot,
-        // edge,
-        swing,
-      ];
+  /// A list of all nonogram.
+  static final List<IsolateNonogram> all = <IsolateNonogram>[
+    dancer,
+    cat,
+    knot,
+    swing,
+  ];
 
+  /// Gets the dancer nonogram.
   static IsolateNonogram get dancer => kDancerIsolate;
+
+  /// Gets the cat nonogram.
   static IsolateNonogram get cat => kCatIsolate;
+
+  /// Gets the knot nonogram.
   static IsolateNonogram get knot => kKnotIsolate;
-// IsolateNonogram get edge => kEdgeIsolate;
+
+  /// Gets the swing nonogram.
   static IsolateNonogram get swing => kSwingIsolate;
 }
