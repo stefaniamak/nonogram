8fb33062059c77780ce57347bccccf7a8c046397
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-12-16 20:59:37 +0200

    adds doc to main

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:nonogram/config/app_theme.dart';
 import 'package:nonogram/pages/game/nonogram_list_page.dart';
 
+/// The main entry point of the application.
 void main() {
   runApp(const MyApp());
 }
 
+/// A stateless widget that represents the root of the application.
 class MyApp extends StatelessWidget {
+  /// Creates a new instance of [MyApp].
   const MyApp({super.key});
 
   @override
