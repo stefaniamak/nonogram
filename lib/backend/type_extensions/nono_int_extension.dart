c42f6e2f48a6ca2851b8b1109bb33ec2964ef8c5
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2024-12-23 21:44:46 +0200

    adds dots to int values (#40)
    * adds dots to values
    
    * changes commas keyword with dots

---
+++ lib/backend/type_extensions/nono_int_extension.dart
@@ -0,0 +1,12 @@
+import 'package:intl/intl.dart';
+
+/// Extension on Int to add utility methods for nonogram puzzles.
+extension NonoIntExtension on int {
+  /// Formats the integer as a string with a thousand separator.
+  ///
+  /// Returns the formatted integer as a string.
+  String get dotsFormatted => _thousandsFormatter.format(this);
+}
+
+/// A NumberFormat object to format integers with a thousand separator.
+final NumberFormat _thousandsFormatter = NumberFormat("###,###.###", "tr_TR");
