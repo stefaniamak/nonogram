a4838fd98df49d3d2d99bc6c39f0f14864f9cd43
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2025-01-07 00:14:43 +0200

    move function to better position within file

--- lib/solver/line_solver_isolate.dart
+++ lib/solver/line_solver_isolate.dart
@@ -320,70 +320,6 @@ IsolateOutput _loopSides({
   );
 }
 
-/// The `getSideMostSolutionsMatches` function identifies the positions of sure boxes to fill or cross out
-/// by comparing the starting and ending most solutions for a given line (row or column) of a nonogram puzzle.
-///
-/// The function receives the following parameters:
-/// - [allLineSolutions]: A list of lists of strings representing all possible solutions for each character in the line.
-/// - [startingMostSolution]: A list of strings representing the starting most solution for the line.
-/// - [endingMostSolution]: A list of strings representing the ending most solution for the line.
-/// - [charIndexesOfQMarks]: A list of integers representing the positions of question marks in the initial solution.
-///
-/// The function finds the clues that have the same positions at both starting and ending most solutions.
-/// It then adds the positions of the sure boxes to fill or cross out to a map, which is returned as the result.
-///
-/// The function returns a map where the keys are the clue indexes and the values are lists of character indexes
-/// representing the positions of sure boxes to fill or cross out.
-Map<int, List<int>> _getSideMostSolutionsMatches(
-  List<List<String>> allLineSolutions,
-  List<String> startingMostSolution,
-  List<String> endingMostSolution,
-  List<int> charIndexesOfQMarks,
-) {
-  // Initialize a map to store the matched positions.
-  //
-  // The key is the character index of the clue on the whole solution.
-  // The value is the clue value plus the number 2 (which is used throughout the
-  // whole solver, to separate the clue numbers from the 0 and 1 filled identifiers).
-  final Map<int, Set<int>> matchMap = <int, Set<int>>{};
-
-  // Find all matches between the starting and ending most solutions.
-  // If a clue number is located at the same character index in both solutions,
-  // add it to the match map.
-  final Set<(int, String)> inputNumbersStart = startingMostSolution.indexed.toSet();
-  final Set<(int, String)> inputNumbersEnd = endingMostSolution.indexed.toSet();
-  final Set<(int, String)> duplicateInputNumbers = inputNumbersStart.intersection(inputNumbersEnd);
-
-  // Iterate over the matched positions and add only the non-zero values ([rightNumber] != 0)
-  // that are missing from the current solution (their position, [leftNumber], is
-  // included in the charIndexesOfQMarks list), to the match map.
-  for (final (int, String) match in duplicateInputNumbers) {
-    final int leftNumber = match.$1;
-    final int rightNumber = int.parse(match.$2);
-    if (rightNumber != 0 && charIndexesOfQMarks.contains(leftNumber)) {
-      matchMap.putIfAbsent(rightNumber, () => <int>{});
-      matchMap[rightNumber]!.add(leftNumber);
-    }
-  }
-
-  // Find all matches for the always zero-filled boxes from all possible solutions.
-  final String inclusionPattern = charIndexesOfQMarks.map((int e) => e).join('|');
-  final RegExp regZeroFilledMatches = RegExp(r'\((' + inclusionPattern + r'), \[(0)\]\)');
-  final String inputZeros = allLineSolutions.indexed.toList().toString();
-  final Iterable<RegExpMatch> matchesZeros = regZeroFilledMatches.allMatches(inputZeros);
-
-  // Iterate over the matched positions and add the zero values to the match map.
-  if (matchesZeros.isNotEmpty) {
-    matchMap.putIfAbsent(0, () => <int>{});
-    matchMap[0]!.addAll(matchesZeros.map((RegExpMatch match) => int.parse(match.group(1)!)));
-  }
-
-  // Convert the match map to a map with integer keys and list values.
-  // The key is the clue value + 2, and the value is a list of character indexes that this clue
-  // exists on its own in both side-most solutions.
-  return matchMap.map((int key, Set<int> value) => MapEntry<int, List<int>>(key, value.toList()));
-}
-
 /// The `getAllLinePossibleSolutions` function calculates all possible solutions for a given line (row or column)
 /// of a nonogram puzzle based on the provided clues and the current state of the solution.
 ///
@@ -549,6 +485,70 @@ List<String> _getSideMostSolution(
 
   // Reverse the side most solution back to the original order if the axis is `NonoAxisAlignment.end`.
   return axis == NonoAxisAlignment.end ? sideMostSolution.reversed.toList() : sideMostSolution;
+}
+
+/// The `getSideMostSolutionsMatches` function identifies the positions of sure boxes to fill or cross out
+/// by comparing the starting and ending most solutions for a given line (row or column) of a nonogram puzzle.
+///
+/// The function receives the following parameters:
+/// - [allLineSolutions]: A list of lists of strings representing all possible solutions for each character in the line.
+/// - [startingMostSolution]: A list of strings representing the starting most solution for the line.
+/// - [endingMostSolution]: A list of strings representing the ending most solution for the line.
+/// - [charIndexesOfQMarks]: A list of integers representing the positions of question marks in the initial solution.
+///
+/// The function finds the clues that have the same positions at both starting and ending most solutions.
+/// It then adds the positions of the sure boxes to fill or cross out to a map, which is returned as the result.
+///
+/// The function returns a map where the keys are the clue indexes and the values are lists of character indexes
+/// representing the positions of sure boxes to fill or cross out.
+Map<int, List<int>> _getSideMostSolutionsMatches(
+  List<List<String>> allLineSolutions,
+  List<String> startingMostSolution,
+  List<String> endingMostSolution,
+  List<int> charIndexesOfQMarks,
+) {
+  // Initialize a map to store the matched positions.
+  //
+  // The key is the character index of the clue on the whole solution.
+  // The value is the clue value plus the number 2 (which is used throughout the
+  // whole solver, to separate the clue numbers from the 0 and 1 filled identifiers).
+  final Map<int, Set<int>> matchMap = <int, Set<int>>{};
+
+  // Find all matches between the starting and ending most solutions.
+  // If a clue number is located at the same character index in both solutions,
+  // add it to the match map.
+  final Set<(int, String)> inputNumbersStart = startingMostSolution.indexed.toSet();
+  final Set<(int, String)> inputNumbersEnd = endingMostSolution.indexed.toSet();
+  final Set<(int, String)> duplicateInputNumbers = inputNumbersStart.intersection(inputNumbersEnd);
+
+  // Iterate over the matched positions and add only the non-zero values ([rightNumber] != 0)
+  // that are missing from the current solution (their position, [leftNumber], is
+  // included in the charIndexesOfQMarks list), to the match map.
+  for (final (int, String) match in duplicateInputNumbers) {
+    final int leftNumber = match.$1;
+    final int rightNumber = int.parse(match.$2);
+    if (rightNumber != 0 && charIndexesOfQMarks.contains(leftNumber)) {
+      matchMap.putIfAbsent(rightNumber, () => <int>{});
+      matchMap[rightNumber]!.add(leftNumber);
+    }
+  }
+
+  // Find all matches for the always zero-filled boxes from all possible solutions.
+  final String inclusionPattern = charIndexesOfQMarks.map((int e) => e).join('|');
+  final RegExp regZeroFilledMatches = RegExp(r'\((' + inclusionPattern + r'), \[(0)\]\)');
+  final String inputZeros = allLineSolutions.indexed.toList().toString();
+  final Iterable<RegExpMatch> matchesZeros = regZeroFilledMatches.allMatches(inputZeros);
+
+  // Iterate over the matched positions and add the zero values to the match map.
+  if (matchesZeros.isNotEmpty) {
+    matchMap.putIfAbsent(0, () => <int>{});
+    matchMap[0]!.addAll(matchesZeros.map((RegExpMatch match) => int.parse(match.group(1)!)));
+  }
+
+  // Convert the match map to a map with integer keys and list values.
+  // The key is the clue value + 2, and the value is a list of character indexes that this clue
+  // exists on its own in both side-most solutions.
+  return matchMap.map((int key, Set<int> value) => MapEntry<int, List<int>>(key, value.toList()));
 }
 
 /// The `_canCluesFit` function checks if a given clue can fit into a specified position in the solution line of a nonogram puzzle.
